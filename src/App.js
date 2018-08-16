c132ef2c86551a7bc5d139400c761e0352decd32
Jeffery Patton <jefferypatton@Jeff-Pattons-MacBook-Pro.local>
2018-08-16 10:22:49 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,169 @@
+import React, { Component } from 'react';
+import Navbar from './components/Navbar';
+import uuid from 'uuid';
+import ContactCard from './components/ContactCard';
+import AddContactForm from './components/AddContactForm';
+import AddEditForm from './components/AddEditForm';
+
+/*
+  Contact Manager
+
+  Application to keep a contact book with people and their information.
+
+  Data about contacts:
+    Id
+    Name
+    Email
+    Phone
+    Notes
+    Company
+    When you met
+
+
+*/
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      contacts: [
+        {
+          id: uuid(),
+          name: 'John Doe',
+          email: 'jdoe@gmail.com',
+          phone: '111-111-1111',
+          notes: 'Generic guy',
+          company: 'Doe Inc',
+          dateMet: '5-6-2015'
+        },
+        {
+          id: uuid(),
+          name: 'Sally Blogs',
+          email: 'sblogs@gmail.com',
+          phone: '222-222-2222',
+          notes: 'Blogger',
+          company: 'weBlog',
+          dateMet: '8-14-2016'
+        },
+        {
+          id: uuid(),
+          name: 'Karen Syles',
+          email: 'sylesk@gmail.com',
+          phone: '333-333-3333',
+          notes: 'Kyles for short',
+          company: 'Starbucks',
+          dateMet: '6-8-2015'
+        },
+        {
+          id: uuid(),
+          name: 'Jack Ford',
+          email: 'ford2@gmail.com',
+          phone: '444-444-4444',
+          notes: 'Not a car guy',
+          company: 'Honda',
+          dateMet: '4-21-2017'
+        }
+      ],
+      route: 'viewContacts'
+    };
+  }
+
+  switchRouteParent = route => {
+    this.setState({ route: route });
+  };
+
+  onSubmit = newContact => {
+    //const newContacts = this.state.contacts.concat([newContact]);
+
+    const newContacts = this.state.contacts;
+    newContacts.push(newContact);
+
+    this.setState({ contacts: newContacts });
+    this.switchRouteParent('viewContacts');
+  };
+
+  onEditSubmit = updateContact => {
+    // find item to update
+
+    let selectContactIndex = this.state.contacts.findIndex(
+      contact => contact.id === updateContact.id
+    );
+
+    // for( let i = 0; i < this.state.contacts.length; i++) {
+    //   if (updateContact.id === this.state.contacts[i].id) {
+    //     selectContactIndex = i;
+    //   }
+    // }
+
+    // replace them
+    const contactsArry = this.state.contacts;
+
+    contactsArry[selectContactIndex] = updateContact;
+
+    // update state
+
+    this.setState({ contacts: contactsArry });
+    this.switchRouteParent('viewContacts');
+  }; // end onEditSubmit
+
+  goToEdit = contact => {
+    this.setState({ currentContact: contact });
+
+    // Route to AddEditForm
+    this.switchRouteParent('AddEditForm');
+  };
+
+  deleteContact = contact => {
+    const newArray = this.state.contacts;
+    let selectContactIndex;
+    for (let i = 0; i < this.state.contacts.length; i++) {
+      if (contact.id === this.state.contacts[i].id) {
+        selectContactIndex = i;
+      }
+    }
+    newArray.splice(selectContactIndex, 1);
+    this.setState({ contacts: newArray });
+  };
+
+  render() {
+    let element;
+
+    switch (this.state.route) {
+      case 'viewContacts':
+        element = (
+          <div>
+            {this.state.contacts.map(contact => (
+              <ContactCard
+                contact={contact}
+                key={contact.id}
+                goToEdit={this.goToEdit}
+                deleteContact={this.deleteContact}
+              />
+            ))}
+          </div>
+        );
+        break;
+      case 'addContact':
+        element = <AddContactForm onSubmitParent={this.onSubmit} />;
+        break;
+      case 'AddEditForm':
+        element = (
+          <AddEditForm
+            onSubmitParent={this.onEditSubmit}
+            contact={this.state.currentContact}
+          />
+        );
+        break;
+      default:
+        element = <div>404 Component not found</div>;
+    }
+
+    return (
+      <div>
+        <Navbar switchRouteChild={this.switchRouteParent} />
+        <div className="container">{element}</div>
+      </div>
+    );
+  }
+}
---
+++ src/components/ContactCard.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+/*
+  Create a contact card to represent each person.
+  Add propTypes for the properties that are expected.
+*/
+export default class ContactCard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      visible: false
+    };
+  }
+
+  toggleDetails = () => {
+    // this.state.visible
+    //   ? this.setState({ visible: false })
+    //   : this.setState({ visible: true });
+
+    this.setState({ visible: !this.state.visible });
+  };
+
+  render() {
+    const { contact } = this.props;
+    let element;
+    if (this.state.visible) {
+      element = (
+        <div className="card-body">
+          <p>
+            <strong>Phone:</strong> {contact.phone}
+          </p>
+          <p>
+            <strong>Email:</strong> {contact.email}
+          </p>
+          <p>
+            <strong>Met:</strong> {contact.dateMet}
+          </p>
+          <p>
+            <strong>Notes:</strong> {contact.notes}
+          </p>
+        </div>
+      );
+    } else {
+      element = null;
+    }
+
+    return (
+      <div className="card">
+        <div className="card-header" onClick={this.toggleDetails}>
+          <h3>{contact.name}</h3>
+          <h4>
+            <em>- {contact.company}</em>
+          </h4>
+        </div>
+        {this.state.visible ? (
+          <div className="card-body">
+            <p>
+              <strong>Phone:</strong> {contact.phone}
+            </p>
+            <p>
+              <strong>Email:</strong> {contact.email}
+            </p>
+            <p>
+              <strong>Met:</strong> {contact.dateMet}
+            </p>
+            <p>
+              <strong>Notes:</strong> {contact.notes}
+            </p>
+            {this.props.goToEdit && (
+              <button
+                className="btn btn-warning"
+                onClick={this.props.goToEdit.bind(this, contact)}
+              >
+                Update Contact
+              </button>
+            )}
+            {this.props.deleteContact && (
+              <button
+                className="btn btn-danger"
+                onClick={this.props.deleteContact.bind(this, contact)}
+              >
+                Delete
+              </button>
+            )}
+          </div>
+        ) : null}
+      </div>
+    );
+  }
+}
+
+ContactCard.propTypes = {
+  contact: PropTypes.object.isRequired,
+  goToEdit: PropTypes.func,
+  deleteContact: PropTypes.func
+};
